8be9243ec5ccc41b9980f067b8c56cbb9bd12fa7
stefan520 <snyg96@gmail.com>
2024-08-26 23:20:21 +0200

    Add logic to SelectJob

--- code/GameSystems/Jobs/JobSystem.cs
+++ code/GameSystems/Jobs/JobSystem.cs
@@ -30,7 +30,37 @@ namespace GameSystems.Jobs
 		// Get default job when player spawns
 		public static JobResource GetDefault()
 		{
-			return ResourceLibrary.Get<JobResource>( "data/jobs/citizen.job" ); ;
+			return ResourceLibrary.Get<JobResource>( "data/jobs/citizen.job" );
+		}
+
+		// Set selected job
+		public void SelectJob( JobResource job, GameObject Player )
+		{
+			// Get all players in the game
+			var players = GameController.Instance.GetAllPlayers();
+
+			// Check how many players have this job
+			var count = 0;
+			foreach ( var player in players )
+			{
+				if ( player.Value.GameObject.Components.Get<Stats>().Job == job )
+				{
+					count++;
+				}
+			}
+
+			Log.Info( $"Count: {count}" );
+
+			// Compare the number of players having this job with the MaxWorkers and there is a possitive value on MaxWorkers
+			// If MaxWorkers is set to -1 then there is no limit 
+			if ( count >= job.MaxWorkers && job.MaxWorkers >= 0 )
+			{
+				Player.Components.Get<Stats>().SendMessage( $"We've reached the maximum amount of workers ({job.MaxWorkers})" );
+				return;
+			}
+
+			// Set player job
+			Player.Components.Get<Stats>().SelectJob( job );
 		}
 	}
 }
